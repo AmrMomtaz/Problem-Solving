6c140bbd1bcac35f49f01c1c1cf1e5f3a8844b64
Amr Momtaz <61145262+AmrMomtaz@users.noreply.github.com>
2022-02-20 18:50:55 +0200

    Greedy solution with sorting is not working

--- JAVA/src/brimore/Brimore.java
+++ JAVA/src/brimore/Brimore.java
@@ -14,22 +14,26 @@ public class Brimore {
         Arrays.sort(numbers);
         long[] results = new long[N-1];
         long[] sums = new long[N];
-        int indexLeft = 1;
-        int indexRight = N-2;
+        int indexLeft = 0;
+        int indexRight = N-1;
+        int index = 1;
         results[0] = numbers[N-1]-numbers[0];
         sums[0] = numbers[0];
         sums[N-1] = numbers[N-1];
         boolean lefTurn = true;
-        while(indexLeft<=indexRight){
+        while(index<N-1){
             if(lefTurn){
-
-                indexLeft++;
+                results[index] = sums[indexRight] - sums[indexLeft] + results[index-1];
+                sums[++indexLeft] = sums[indexLeft-1] + numbers[indexLeft];
+                lefTurn= false;
             }else{
-                
-                indexRight--;
+                results[index] = sums[indexRight] - sums[indexLeft] + numbers[indexRight-1] +results[index-1];
+                sums[--indexRight] = sums[indexRight+1] + numbers[indexRight];
+                lefTurn = true;
             }
+            index++;
         }
-        for(int result : results)
+        for(long result : results)
             System.out.print(result + " ");
     }
 }
