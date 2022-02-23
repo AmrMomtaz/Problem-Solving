7185c1aedebf28108e66cc359e147d97f26e4c3d
Amr Momtaz <61145262+AmrMomtaz@users.noreply.github.com>
2022-02-23 03:13:48 +0200

    Long version
    I just need to maintain a tree instead of array

--- JAVA/src/brimore/Contest_Day.java
+++ JAVA/src/brimore/Contest_Day.java
@@ -1,45 +1,42 @@
 package brimore;
 
 import java.io.*;
-import java.math.BigInteger;
-import java.util.Arrays;
+
 import java.util.Hashtable;
 
 public class Contest_Day {
 
     static class FenwickTree{
 
-        public static BigInteger getSum(int index,BigInteger[] BITree){
-            BigInteger sum = BigInteger.ZERO;
+        public static long getSum(int index,long[] BITree){
+            long sum = 0;
             index++;
             while(index>0) {
-                sum = sum.add(BITree[index]);
+                sum +=BITree[index];
                 index -= index & (-index);
             }
             return sum;
         }
 
-        public static void updateBIT(int index,long val,BigInteger[] BITree){
-            BigInteger bigVal = BigInteger.valueOf(val).
-                    subtract(getSum(index, BITree).subtract(getSum(index-1, BITree)));
+        public static void updateBIT(int index,long val,long[] BITree){
+            val -= getSum(index, BITree) - getSum(index-1, BITree);
             index++;
             while(index < BITree.length) {
-                BITree[index] = BITree[index].add(bigVal);
+                BITree[index] += val;
                 index += index & (-index);
             }
         }
 
-        public static void updateBITConstruction(int index,long val,BigInteger[] BITree){
+        public static void updateBITConstruction(int index,long val,long[] BITree){
             index++;
             while(index < BITree.length){
-                BITree[index] = BITree[index].add(BigInteger.valueOf(val));
+                BITree[index] += val;
                 index += index & (-index);
             }
         }
 
-        public static BigInteger[] constructBITree(long[] arr){
-            BigInteger[] BITree = new BigInteger[arr.length+1];
-            Arrays.fill(BITree, BigInteger.ZERO);
+        public static long[] constructBITree(int[] arr){
+            long[] BITree = new long[arr.length+1];
             for(int i = 0; i < arr.length; i++)
                 updateBITConstruction(i, arr[i], BITree);
             return BITree;
@@ -52,18 +49,18 @@ public class Contest_Day {
         String[] input = br.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
         int Q = Integer.parseInt(input[1]);
-        long[] arr = new long[N];
+        int[] arr = new int[N];
         input = br.readLine().split(" ");
         for (int i = 0; i < N; i++)
-            arr[i] = Long.parseLong(input[i]);
-        Hashtable<Integer, BigInteger[]> hashtable = new Hashtable<>();
+            arr[i] = Integer.parseInt(input[i]);
+        Hashtable<Integer, long[]> hashtable = new Hashtable<>();
         hashtable.put(1,FenwickTree.constructBITree(arr));
         int index = 2;
         for(int i = 0 ; i < Q ; i++){
             input = br.readLine().split(" ");
             int OP = Integer.parseInt(input[0]);
             int k = Integer.parseInt(input[1]);
-            BigInteger[] BITree = hashtable.get(k);
+            long[] BITree = hashtable.get(k);
             switch (OP) {
                 case 1:
                     int A = Integer.parseInt(input[2])-1;
@@ -76,7 +73,7 @@ public class Contest_Day {
                     if (s == 0)
                         bw.write(FenwickTree.getSum(e, BITree) + "\n");
                     else
-                        bw.write((FenwickTree.getSum(e, BITree).subtract(FenwickTree.getSum(s-1, BITree))) + "\n");
+                        bw.write((FenwickTree.getSum(e, BITree)-FenwickTree.getSum(s-1, BITree)) + "\n");
                     break;
                 case 3:
                     hashtable.put(index, BITree.clone());
