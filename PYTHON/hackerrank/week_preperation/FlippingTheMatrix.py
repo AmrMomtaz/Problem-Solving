109eed54d736813b6480858bd56ce9892b37f5a8
Amr Momtaz <61145262+AmrMomtaz@users.noreply.github.com>
2022-04-29 22:10:34 +0200

    Modify

--- PYTHON/hackerrank/week_preperation/FlippingTheMatrix.py
+++ PYTHON/hackerrank/week_preperation/FlippingTheMatrix.py
@@ -7,7 +7,6 @@ import re
 import sys
 
 
-
 #
 # Complete the 'flippingMatrix' function below.
 #
@@ -16,24 +15,15 @@ import sys
 #
 
 def flippingMatrix(matrix):
-    mid = int(len(matrix)/2)
-    for i in range(mid):
-        if sum(matrix[i][mid:])> sum(matrix[i][:mid]):
-            matrix[i] = matrix[i][mid:] + matrix[i][:mid]
-    for i in range(mid,len(matrix)):
-        if sum(matrix[i][mid:])> sum(matrix[i][:mid]):
-            matrix[i] = matrix[i][mid:] + matrix[i][:mid]
+    mid = int(len(matrix) / 2)
+    n = len(matrix) - 1
     total = 0
-    for j in range(mid):
-        sum_up = 0
-        sum_down = 0
-        for i in range(len(matrix)):
-            if i < mid :
-                sum_up += matrix[i][j]
-            else:
-                sum_down += matrix[i][j]
-        total += max(sum_up,sum_down)
+    for i in range(mid):
+        for j in range(mid):
+            total += max(matrix[i][j], matrix[i][n - j], matrix[n - i][j], matrix[n - i][n - j])
     return total
+
+
 if __name__ == '__main__':
     fptr = open('output.txt', 'w')
 
